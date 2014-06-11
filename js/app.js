15286038f037397f467a78a4507a474fa6bab86b
100306062 <100306062@nccu.edu.tw>
2014-06-11 15:59:31 +0800

    personal key added

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 (function () {
-  Parse.initialize("s4747jSandiQGpXlbizjGZMgXJAepvnGnbc7NCmu",
-    "PihDCqy36lpBPQc8rmiEH477nSD0tT1vrSSjBAm0");
+  Parse.initialize("PNg01BS19GIgtSymZXCzv8XkDSPAlEY9agU6z1Sc",
+    "hqCvtaEqquzNMViKdlEikivNJrIXaCtJWD6LX5EO"); //初始化Parse
   var templates = {};
   ["loginView", "evaluationView", "updateSuccessView"].forEach(function (e) {
     templateCode = document.getElementById(e).text;
-    templates[e] = doT.template(templateCode);
+    templates[e] = doT.template(templateCode); //將各版型compile並載入記憶體中
   });
 
   var commons = {
     loginRequiredView: function (ViewFunction) {
       return function () {
-        var currentUser = Parse.User.current();
+        var currentUser = Parse.User.current();//當前使用者判定
         if (currentUser) {
           ViewFunction();
         } else {
@@ -21,31 +21,31 @@
   }
 
   var handlers = {
-    navbar: function () {
-      var currentUser = Parse.User.current();
-      if (currentUser) {
-        document.getElementById("loginButton").style.display = "none";
-        document.getElementById("logoutButton").style.display = "block";
-        document.getElementById("evaluationButton").style.display = "block";
-      } else {
-        document.getElementById("loginButton").style.display = "block";
-        document.getElementById("logoutButton").style.display = "none";
-        document.getElementById("evaluationButton").style.display = "none";
+    navbar: function () { //navigation bar之狀態切換
+      var currentUser = Parse.User.current(); 
+      if (currentUser) { //如為該位使用者
+        document.getElementById("loginButton").style.display = "none"; //不顯示登入按鈕
+        document.getElementById("logoutButton").style.display = "block"; //顯示登出按鈕
+        document.getElementById("evaluationButton").style.display = "block";//顯示評分表
+      } else { //不為該位使用者
+        document.getElementById("loginButton").style.display = "block"; //顯示登入按鈕
+        document.getElementById("logoutButton").style.display = "none"; //不顯示登出按鈕
+        document.getElementById("evaluationButton").style.display = "none"; //不顯示評分表
       }
-      document.getElementById("logoutButton").addEventListener('click', function () {
-        Parse.User.logOut();
-        handlers.navbar();
+      document.getElementById("logoutButton").addEventListener('click', function () { //按鈕點擊事件
+        Parse.User.logOut(); //登出
+        handlers.navbar(); //navbar改變
         window.location.hash = 'login/';
       });
     },
-    evaluationView: commons.loginRequiredView(function () {
-      var Evaluation = Parse.Object.extend('Evaluation');
+    evaluationView: commons.loginRequiredView(function () { //評分view()
+      var Evaluation = Parse.Object.extend('Evaluation');//設定Class-Based權限
       var currentUser = Parse.User.current();      
-      var evaluationACL = new Parse.ACL();
-      evaluationACL.setPublicReadAccess(false);
-      evaluationACL.setPublicWriteAccess(false);
-      evaluationACL.setReadAccess(currentUser, true);
-      evaluationACL.setWriteAccess(currentUser, true);
+      var evaluationACL = new Parse.ACL();//設定ACL全縣
+      evaluationACL.setPublicReadAccess(false);//設定只有開發者能讀取此ACL
+      evaluationACL.setPublicWriteAccess(false);//設定只有開發者能編輯此ACL
+      evaluationACL.setReadAccess(currentUser, true);//設定只有當前使用者能讀取此ACL
+      evaluationACL.setWriteAccess(currentUser, true);//設定只有當前使用者能讀取此ACL
       var query = new Parse.Query(Evaluation);
       query.equalTo('user', currentUser);
       query.first({
@@ -91,8 +91,10 @@
         }
       }); 
     }),
-    loginView: function (redirect) {
-      var checkVaildStudentID = function(DOM_ID) {
+	
+	
+    loginView: function (redirect) { //登入view函數
+      var checkVaildStudentID = function(DOM_ID) { //綁定登入表單的學號檢查事件()
         var student_ID = document.getElementById(DOM_ID).value;
         return (TAHelp.getMemberlistOf(student_ID) === false) ? false : true;
       }
@@ -104,11 +106,11 @@
           document.getElementById(DOM_ID).style.display = "none";
         }
       }
-      var postAction = function() {
+      var postAction = function() { //改變navbar
         handlers.navbar();
         window.location.hash = (redirect) ? redirect : '';
       }
-      var passwordMatch = function(){
+      var passwordMatch = function(){ // 綁定註冊表單的密碼檢查事件(); 
         var singupForm_password = document.getElementById('form-signup-password');
         var singupForm_password1 = document.getElementById('form-signup-password1');
         var BOOL = (singupForm_password.value === singupForm_password1.value) ? true : false;
@@ -126,7 +128,8 @@
           alert("The student is not one of the class students.");
           return false;
         }
-        Parse.User.logIn(document.getElementById("form-signin-student-id").value,
+		
+        Parse.User.logIn(document.getElementById("form-signin-student-id").value, //綁定登入表單的登入檢查事件()
           document.getElementById("form-signin-password").value, {
             success: function(user) {
               postAction();
@@ -134,7 +137,7 @@
             error: function (user, error) {
               showMessage('form-signin-message', function () {
                 return false;
-              }, "Invaild username or password.");
+              }, "Invaild username or password.");//失敗則回傳
             }
           });
       }, false);
@@ -154,7 +157,7 @@
           return false;
         }
 
-        var user = new Parse.User();
+        var user = new Parse.User(); //綁定註冊表單的註冊檢查事件()
         user.set("username", document.getElementById('form-signup-student-id').value);
         user.set("password", document.getElementById('form-signup-password').value);
         user.set("email", document.getElementById('form-signup-email').value);
@@ -174,16 +177,16 @@
 
   var Router = Parse.Router.extend({
     routes: {
-      "": "indexView",
+      "": "indexView",   //路徑規劃和處理函數
       "peer-evaluation/": "evaluationView",
       "login/*redirect": "loginView",
     },
-    indexView: handlers.evaluationView,
+    indexView: handlers.evaluationView, //處理函數名稱&函數本體
     evaluationView: handlers.evaluationView,
     loginView: handlers.loginView,
   });
 
-  this.Router = new Router();
+  this.Router = new Router(); //讓router活起來();
   Parse.history.start();
   handlers.navbar();
 
